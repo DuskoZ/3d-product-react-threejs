32e7cf3bfd1f7f1fae78f50a056f87a60f880e60
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-05-04 16:00:47 +0200

    updated page markup

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -30,6 +30,33 @@ const Customizer = () => {
         stylishShirt: false,
     });
 
+    // show tab content depending on the activeTab
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case "colorpicker":
+                return <ColorPicker />;
+            case "filepicker":
+                return (
+                    <FilePicker
+                        file={file}
+                        setFile={setFile}
+                        readFile={readFile}
+                    />
+                );
+            case "aipicker":
+                return (
+                    <AIPicker
+                        prompt={prompt}
+                        setPrompt={setPrompt}
+                        generatingImg={generatingImg}
+                        handleSubmit={handleSubmit}
+                    />
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
         <AnimatePresence>
             {!snap.intro && (
@@ -46,9 +73,13 @@ const Customizer = () => {
                                     <Tab
                                         key={tab.name}
                                         tab={tab}
-                                        handleClick={() => {}}
+                                        handleClick={() =>
+                                            setActiveEditorTab(tab.name)
+                                        }
                                     />
                                 ))}
+
+                                {generateTabContent()}
                             </div>
                         </div>
                     </motion.div>
